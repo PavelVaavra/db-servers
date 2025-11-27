954b2b0f299570dd9a11d6b3c911c2de8a68b773
devuser <devuser@debian-test.brdev.net>
2025-11-27 13:22:32 +0100

    command to start mapp Database script in container

--- docker-cmds.sh
+++ docker-cmds.sh
@@ -4,3 +4,5 @@ docker compose up -d
 docker compose down -v
 # start psql inside container
 docker exec -it my_postgres psql -U myuser -d mydb
+# start mapp Database script
+docker run --name mappdatabase-01 --restart=always -d -p 85:85 -e DB_HOST=debian-test.brdev.net -e DB_PORT=5432 -e POLL_INTERVAL=20 th0masis/mappdatabase:6_0
