3d8f44c46a96362e57d04e892e1d757eae967b99
devuser <devuser@debian-test.brdev.net>
2025-12-02 12:52:06 +0100

    commands to run mapp Database scripts for mysql and mssql

--- docker-cmds.sh
+++ docker-cmds.sh
@@ -6,5 +6,9 @@ docker compose down -v
 docker exec -it my_postgres psql -U myuser -d mydb
 # start mysql client inside container
 docker exec -it my_mysql mysql -u myuser -pmypassword -D mydb
-# start mapp Database script
+# start mapp Database script for Postgres
 docker run --name mappdatabase-01 --restart=always -d -p 85:85 -e DB_HOST=debian-test.brdev.net -e DB_PORT=5432 -e POLL_INTERVAL=20 th0masis/mappdatabase:6_0
+# start mapp Database script for MySql
+docker run --name mappdatabase-01 --restart=always -d -p 85:85 -e DB_HOST=debian-test.brdev.net -e DB_PORT=3306 -e POLL_INTERVAL=20 th0masis/mappdatabase:6_0
+# start mapp Database script for MsSql
+docker run --name mappdatabase-01 --restart=always -d -p 85:85 -e DB_HOST=debian-test.brdev.net -e DB_PORT=1433 -e POLL_INTERVAL=20 th0masis/mappdatabase:6_0
